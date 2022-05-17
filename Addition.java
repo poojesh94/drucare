45f8db825be3f092c498980e2ca735cd916480fa
poojesh94 <105488064+poojesh94@users.noreply.github.com>
2022-05-17 11:55:16 +0530

    Program

---
+++ Addition.java
@@ -0,0 +1,17 @@
+package demo2;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=20,b=10;
+	  System.out.println(a+b);
+	  System.out.println(a*b);
+	  System.out.println(a/b);
+	  System.out.println(a%b);
+	  
+	}
+
+}
