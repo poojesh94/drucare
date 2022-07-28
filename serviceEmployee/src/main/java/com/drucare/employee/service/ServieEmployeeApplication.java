7627518a5eb34ad482080aff781726b4fe2abb4f
poojesh94 <105488064+poojesh94@users.noreply.github.com>
2022-07-28 19:24:29 +0530

    Add files via upload

---
+++ serviceEmployee/src/main/java/com/drucare/employee/service/ServieEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.drucare.employee.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServieEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServieEmployeeApplication.class, args);
+	}
+
+}
---
+++ serviceEmployee/src/test/java/com/drucare/employee/service/ServieEmployeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drucare.employee.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServieEmployeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
