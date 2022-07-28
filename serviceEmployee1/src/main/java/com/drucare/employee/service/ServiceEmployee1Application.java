b4afe17c642315c7c8d9f038ff4745bc96144915
poojesh94 <105488064+poojesh94@users.noreply.github.com>
2022-07-28 19:26:34 +0530

    Add files via upload

---
+++ serviceEmployee1/src/main/java/com/drucare/employee/service/ServiceEmployee1Application.java
@@ -0,0 +1,13 @@
+package com.drucare.employee.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceEmployee1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceEmployee1Application.class, args);
+	}
+
+}
